5fdf311620d22646028c695688a3251bd84fd5e1
Nitish Kanade <nitishkanade@Nitishs-MacBook-Pro.local>
2020-06-11 21:58:23 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import styles from "./App.css";
+import axios from "axios";
 
 function App() {
+  const [advice, setAdvice] = useState("");
+
+  useEffect(() => {
+    fetchAdvice();
+  }, []);
+
+  const fetchAdvice = async () => {
+    try {
+      const { data } = await axios.get("https://api.adviceslip.com/advice");
+      setAdvice(data.slip.advice);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleClick = () => {
+    fetchAdvice();
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="card">
+        <h1 className="heading">{advice}</h1>
+        <button className="button" onClick={handleClick}>
+          <span>GIVE ME ADVICE</span>
+        </button>
+      </div>
     </div>
   );
 }
